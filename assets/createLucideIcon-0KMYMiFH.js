ad26ea23891ec7c1a8d87cdb4c052bd7000b2934
Gingene <ginx2618@gmail.com>
2024-02-11 10:27:53 +0800

    Updates

--- assets/createLucideIcon-0KMYMiFH.js
+++ assets/createLucideIcon-0KMYMiFH.js
@@ -1,4 +1,4 @@
-import{as as a}from"./index-f9igEiiO.js";/**
+import{aD as a}from"./index-rYy_S2Kx.js";/**
  * @license lucide-vue-next v0.314.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/useTheme-Naz0E8FN.js
+++
@@ -1,1 +0,0 @@
-import{G as l}from"./index-f9igEiiO.js";function m(){const a=l(!0),s=l(""),e=document.querySelector(":root");return{toggleDarkTheme:t=>{a.value=t,a.value?e.classList.add("dark"):e.classList.remove("dark")},toggleTheme:t=>{s.value=t,e.className="",a.value&&(e==null||e.classList.add("dark")),s.value!=="theme-default"&&(e==null||e.classList.add(s.value))}}}export{m as u};
