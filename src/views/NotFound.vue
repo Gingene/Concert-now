9447deba4e552688298f200442cab89f67dadbb2
Gingene <ginx2618@gmail.com>
2024-02-13 11:05:12 +0800

    feat: 🎸 新增footer-nav和footer和更新部分上方導覽列

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,11 @@
 <template>
   <HeaderNav />
   <section class="container min-h-screen flex justify-center items-center">
-    <div class="">
+    <div>
       <div>這裡看不到演唱會資料喔~</div>
 
       <div>
-        <Button variant="default">
+        <Button variant="btn1" size="btn1">
           <RouterLink to="/"> 點我回首頁 </RouterLink>
         </Button>
       </div>
@@ -17,12 +17,14 @@ import { Button } from '@/components/ui/button';
 </script>
 <script>
 import HeaderNav from '@/components/layout/HeaderNav.vue';
+import useTheme from '@/hooks/useTheme';
+const { toggleDarkTheme, toggleTheme } = useTheme();
 
 export default {
   components: { HeaderNav },
-  props: ['route'],
-  created() {
-    console.log(this.route);
+  mounted() {
+    toggleDarkTheme(true);
+    toggleTheme('theme-default');
   },
 };
 </script>
