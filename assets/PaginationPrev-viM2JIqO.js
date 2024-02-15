821ea056795b0dd831bec240a8623732292fba56
Gingene <ginx2618@gmail.com>
2024-02-15 14:35:07 +0800

    Updates

---
+++ assets/PaginationPrev-viM2JIqO.js
@@ -0,0 +1,16 @@
+import{l as c,a2 as l,ap as y,o as i,h as p,w as t,z as u,d as o,u as e,B as w,C as g,aq as C,k as f,a3 as h,a4 as _,ar as k,as as q}from"./index-Npp680ri.js";/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=c("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=c("ChevronRightIcon",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=c("MoreHorizontalIcon",[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]]),$=Object.assign({inheritAttrs:!1},{__name:"PaginationEllipsis",props:{asChild:{type:Boolean,required:!1},as:{type:null,required:!1}},setup(s){const a=l(s),{class:r,...n}=y();return(m,z)=>(i(),p(e(C),w({class:e(g)("w-9 h-9 flex items-center justify-center",e(r)??"")},{...e(a),...n}),{default:t(()=>[u(m.$slots,"default",{},()=>[o(e(P))])]),_:3},16,["class"]))}}),I={__name:"PaginationNext",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=l(s);return(r,n)=>(i(),p(e(k),h(_(e(a))),{default:t(()=>[o(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:t(()=>[u(r.$slots,"default",{},()=>[o(e(x),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}},j={__name:"PaginationPrev",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=l(s);return(r,n)=>(i(),p(e(q),h(_(e(a))),{default:t(()=>[o(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:t(()=>[u(r.$slots,"default",{},()=>[o(e(v),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}};export{j as _,I as a,$ as b};
