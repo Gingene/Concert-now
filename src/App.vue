37fb9fccc59c56175cc61ada95bdab8b9ad39752
Gingene <ginx2618@gmail.com>
2024-02-17 21:08:48 +0800

    feat: 🎸 新增toast-UI並新增FormTest元件做為參考

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <LoadingComponent />
   <RouterView />
+  <Toaster />
 </template>
+
+<script setup>
+import Toaster from '@/components/ui/toast/Toaster.vue';
+</script>
 
 <script>
 import LoadingComponent from './components/layout/LoadingComponent.vue';
