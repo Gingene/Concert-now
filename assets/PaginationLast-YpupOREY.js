8cf619b968aaf29afb06c8e96d6ed6cf7d3298aa
Gingene <ginx2618@gmail.com>
2024-02-26 10:00:34 +0800

    Updates

---
+++ assets/PaginationLast-YpupOREY.js
@@ -0,0 +1,11 @@
+import{j as n,a4 as l,o as p,h as c,w as a,b as s,u as e,l as d,p as u,q as i,s as f,b8 as m,b9 as w}from"./index-5s7QOI7o.js";/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=n("ChevronsLeftIcon",[["path",{d:"m11 17-5-5 5-5",key:"13zhaf"}],["path",{d:"m18 17-5-5 5-5",key:"h8a8et"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=n("ChevronsRightIcon",[["path",{d:"m6 17 5-5-5-5",key:"xnjwq"}],["path",{d:"m13 17 5-5-5-5",key:"17xmmf"}]]),k={__name:"PaginationFirst",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(t){const r=l(t);return(o,_)=>(p(),c(e(m),i(f(e(r))),{default:a(()=>[s(e(d),{class:"w-10 h-10 p-0",variant:"outline"},{default:a(()=>[u(o.$slots,"default",{},()=>[s(e(y),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}},q={__name:"PaginationLast",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(t){const r=l(t);return(o,_)=>(p(),c(e(w),i(f(e(r))),{default:a(()=>[s(e(d),{class:"w-10 h-10 p-0",variant:"outline"},{default:a(()=>[u(o.$slots,"default",{},()=>[s(e(v),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}};export{k as _,q as a};
---
+++ assets/PaginationPrev-l0MQiy4g.js
@@ -0,0 +1,11 @@
+import{j as d,a4 as n,ba as y,o as l,h as i,w as t,p,b as o,u as e,x as w,y as b,bb as g,l as f,q as _,s as h,bc as x,bd as C}from"./index-5s7QOI7o.js";import{C as q}from"./chevron-right-DOoupedu.js";/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=d("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=d("MoreHorizontalIcon",[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]]),j=Object.assign({inheritAttrs:!1},{__name:"PaginationEllipsis",props:{asChild:{type:Boolean,required:!1},as:{type:null,required:!1}},setup(s){const a=n(s),{class:r,...c}=y();return(m,k)=>(l(),i(e(g),w({class:e(b)("w-9 h-9 flex items-center justify-center",e(r)??"")},{...e(a),...c}),{default:t(()=>[p(m.$slots,"default",{},()=>[o(e(P))])]),_:3},16,["class"]))}}),z={__name:"PaginationNext",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=n(s);return(r,c)=>(l(),i(e(x),_(h(e(a))),{default:t(()=>[o(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:t(()=>[p(r.$slots,"default",{},()=>[o(e(q),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}},H={__name:"PaginationPrev",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=n(s);return(r,c)=>(l(),i(e(C),_(h(e(a))),{default:t(()=>[o(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:t(()=>[p(r.$slots,"default",{},()=>[o(e(v),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}};export{H as _,j as a,z as b};
