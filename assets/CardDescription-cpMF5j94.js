bfcd84af823bd7105061ff32331beeb54dbf33fd
Gingene <ginx2618@gmail.com>
2024-03-07 15:01:32 +0800

    Updates

---
+++ assets/CardDescription-cpMF5j94.js
@@ -0,0 +1,1 @@
+import{o as a,c as r,L as o,D as n,u as c,Q as l}from"./index-L6S9KTCk.js";const m={__name:"CardDescription",props:{class:{type:String,default:""}},setup(s){const e=s;return(t,p)=>(a(),r("p",{class:n(c(l)("text-sm text-muted-foreground",e.class))},[o(t.$slots,"default")],2))}};export{m as _};
