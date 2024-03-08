1da48c59b856ab9f23423973051cad2e0f8087ea
Gingene <ginx2618@gmail.com>
2024-03-09 00:59:31 +0800

    Updates

--- assets/more-horizontal--NAx5pB5.js
+++
@@ -1,6 +0,0 @@
-import{i as c}from"./index-VAeGz4pi.js";/**
- * @license lucide-vue-next v0.314.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=c("MoreHorizontalIcon",[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]]);export{e as M};
