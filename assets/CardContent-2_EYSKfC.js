dfdd1efdf0e71c87522643d4cd69ffe50c84f77d
Gingene <ginx2618@gmail.com>
2024-03-14 02:40:26 +0800

    Updates

--- assets/CardContent-2_EYSKfC.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as r,P as n,C as o,u as c,U as l}from"./index-OrsFlzd2.js";const m={__name:"CardContent",props:{class:{type:String,default:""}},setup(s){const a=s;return(e,p)=>(t(),r("div",{class:o(c(l)("p-6 pt-0",a.class))},[n(e.$slots,"default")],2))}};export{m as _};
--- assets/CardDescription-pKZmjXDF.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as r,P as o,C as n,u as c,U as l}from"./index-OrsFlzd2.js";const m={__name:"CardDescription",props:{class:{type:String,default:""}},setup(s){const e=s;return(t,p)=>(a(),r("p",{class:n(c(l)("text-sm text-muted-foreground",e.class))},[o(t.$slots,"default")],2))}};export{m as _};
