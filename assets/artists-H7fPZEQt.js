5ae1bb069bec5bbc1e1d0a0bbeab2423ccc278cf
Gingene <ginx2618@gmail.com>
2024-03-13 22:46:52 +0800

    Updates

---
+++ assets/artists-H7fPZEQt.js
@@ -0,0 +1,1 @@
+import{ax as e,ay as r,az as a}from"./index-hYb8mV10.js";const c=e("artists",{state:()=>({}),actions:{async postFollowConcetsData(o){try{await r(o)}catch(t){console.error(t)}},async deleteFollowConcetsData(o){try{await a(o)}catch(t){console.error(t)}}}});export{c as u};
