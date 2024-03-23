2e46ce4bb09bd6e174521bd322b5e26fba8a8e0f
Gingene <ginx2618@gmail.com>
2024-03-23 10:26:52 +0800

    Updates

--- assets/TableCaption-lXwSlnOZ.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as r,P as o,C as n,u as c,U as l}from"./index-WU9oAnbs.js";const u={__name:"TableCaption",props:{class:{type:String,required:!1}},setup(e){const s=e;return(t,p)=>(a(),r("caption",{class:n(c(l)("mt-4 text-sm text-muted-foreground",s.class))},[o(t.$slots,"default")],2))}};export{u as _};
