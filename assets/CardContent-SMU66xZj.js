6020210065410b40466db0d381ae0013d2735f3b
Celine-Lin <celine41104@gmail.com>
2024-04-01 01:14:15 +0200

    Updates

---
+++ assets/CardContent-SMU66xZj.js
@@ -0,0 +1,1 @@
+import{o as t,c as r,P as n,C as o,y as c,U as l}from"./index-4xTLKw1w.js";const u={__name:"CardContent",props:{class:{type:String,default:""}},setup(s){const a=s;return(e,p)=>(t(),r("div",{class:o(c(l)("p-6 pt-0",a.class))},[n(e.$slots,"default")],2))}};export{u as _};
