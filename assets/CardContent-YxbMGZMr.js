9262284b3c687572144db53a7b8f7db87fdd2944
Gingene <ginx2618@gmail.com>
2024-04-03 17:19:36 +0800

    Updates

---
+++ assets/CardContent-YxbMGZMr.js
@@ -0,0 +1,1 @@
+import{o as t,c as r,P as n,C as o,y as c,U as l}from"./index-WpFvv7PG.js";const u={__name:"CardContent",props:{class:{type:String,default:""}},setup(s){const a=s;return(e,p)=>(t(),r("div",{class:o(c(l)("p-6 pt-0",a.class))},[n(e.$slots,"default")],2))}};export{u as _};
