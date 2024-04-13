1bd4316ff4d9c6db1dbb0e5d5634cc954da36427
Gingene <ginx2618@gmail.com>
2024-04-13 22:49:07 +0800

    Updates

--- assets/CardDescription-uDsvdExx.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as r,Q as o,D as n,z as c,V as l}from"./index-DfLsDum5.js";const u={__name:"CardDescription",props:{class:{type:String,default:""}},setup(s){const e=s;return(t,p)=>(a(),r("p",{class:n(c(l)("text-sm text-muted-foreground",e.class))},[o(t.$slots,"default")],2))}};export{u as _};
