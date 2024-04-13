2058a2873d595747718cc26667a2cdc17af9e00f
Celine510 <celinewu1010@gmail.com>
2024-04-13 18:24:11 +0800

    Updates

---
+++ assets/CardDescription-X9Y_nHN4.js
@@ -0,0 +1,1 @@
+import{o as a,c as r,Q as o,D as n,z as c,V as l}from"./index-OA7eRsB3.js";const u={__name:"CardDescription",props:{class:{type:String,default:""}},setup(s){const e=s;return(t,p)=>(a(),r("p",{class:n(c(l)("text-sm text-muted-foreground",e.class))},[o(t.$slots,"default")],2))}};export{u as _};
