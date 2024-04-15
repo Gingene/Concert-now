705a819d5e4a87cb1986c1c6f8b71cd83ae587fc
Celine510 <celinewu1010@gmail.com>
2024-04-15 14:39:39 +0800

    Updates

---
+++ assets/PaginationPrev-MHPXdELV.js
@@ -0,0 +1,21 @@
+import{h as d,aB as l,bi as w,o as c,f as p,w as r,Q as u,b as n,z as e,U as y,V as C,bj as v,C as f,R as h,S as _,bk as g,bl as q,bm as $,bn as b}from"./index-Pe8GTFti.js";import{M as k}from"./more-horizontal-_5iuDUcL.js";/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=d("ChevronLeftIcon",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=d("ChevronRightIcon",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=d("ChevronsLeftIcon",[["path",{d:"m11 17-5-5 5-5",key:"13zhaf"}],["path",{d:"m18 17-5-5 5-5",key:"h8a8et"}]]);/**
+ * @license lucide-vue-next v0.314.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const L=d("ChevronsRightIcon",[["path",{d:"m6 17 5-5-5-5",key:"xnjwq"}],["path",{d:"m13 17 5-5-5-5",key:"17xmmf"}]]),z=Object.assign({inheritAttrs:!1},{__name:"PaginationEllipsis",props:{asChild:{type:Boolean,required:!1},as:{type:null,required:!1}},setup(s){const a=l(s),{class:t,...o}=w();return(m,R)=>(c(),p(e(v),y({class:e(C)("w-9 h-9 flex items-center justify-center",e(t)??"")},{...e(a),...o}),{default:r(()=>[u(m.$slots,"default",{},()=>[n(e(k))])]),_:3},16,["class"]))}}),N={__name:"PaginationFirst",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=l(s);return(t,o)=>(c(),p(e(g),h(_(e(a))),{default:r(()=>[n(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:r(()=>[u(t.$slots,"default",{},()=>[n(e(x),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}},A={__name:"PaginationLast",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=l(s);return(t,o)=>(c(),p(e(q),h(_(e(a))),{default:r(()=>[n(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:r(()=>[u(t.$slots,"default",{},()=>[n(e(L),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}},H={__name:"PaginationNext",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=l(s);return(t,o)=>(c(),p(e($),h(_(e(a))),{default:r(()=>[n(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:r(()=>[u(t.$slots,"default",{},()=>[n(e(B),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}},M={__name:"PaginationPrev",props:{asChild:{type:Boolean,required:!1,default:!0},as:{type:null,required:!1}},setup(s){const a=l(s);return(t,o)=>(c(),p(e(b),h(_(e(a))),{default:r(()=>[n(e(f),{class:"w-10 h-10 p-0",variant:"outline"},{default:r(()=>[u(t.$slots,"default",{},()=>[n(e(P),{class:"h-4 w-4"})])]),_:3})]),_:3},16))}};export{N as _,M as a,z as b,H as c,A as d};
--- assets/artists-23UOd9Wa.js
+++
@@ -1,1 +0,0 @@
-import{aq as e,ar as r,as as s}from"./index-D7IJI3tB.js";const c=e("artists",{state:()=>({}),actions:{async postFollowConcetsData(o){try{await r(o)}catch(t){console.error(t)}},async deleteFollowConcetsData(o){try{await s(o)}catch(t){console.error(t)}}}});export{c as u};
