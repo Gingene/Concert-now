1b7c865c097bdb797c9e06132a6a6cf9ea3c494b
Gingene <ginx2618@gmail.com>
2025-03-19 08:37:44 +0800

    fix: 🐛 導覽列搜尋視窗輪播效果異常

--- src/components/layout/HeaderNav/CarouselList.vue
+++ src/components/layout/HeaderNav/CarouselList.vue
@@ -4,14 +4,14 @@
     <RouterLink :to="carouselData.link" class="absolute top-3 right-0">
       <Button variant="white-outline" size="base" class="text-sm sm:text-base" @click="closeModal">查看更多</Button>
     </RouterLink>
-    <Carousel class="w-[120px] md:w-[500px] lg:w-[750px] xl:w-[85%] 2xl:w-[90%] mt-16">
+    <Carousel :opts="{ align: 'start' }" class="w-[120px] md:w-[500px] lg:w-[750px] xl:w-[950px] 2xl:w-[90%] mt-16">
       <CarouselContent>
         <CarouselItem v-for="searchItem in searchResults" :key="searchItem.id" :class="carouselData.carouselClassName">
           <RouterLink :to="`${carouselData.link}/${searchItem.id}`" class="flex flex-col justify-center items-center" @click="closeModal">
             <div class="mt-2 flex flex-col justify-center items-center text-muted-foreground">
               <slot
                 :image="searchItem?.cover_urls?.square || searchItem?.picture.square"
-                :imageClassName="carouselData.imageClassName"
+                :imageClassName="carouselData?.imageClassName"
                 :searchItemName="searchItem?.name"
                 :title="searchItem?.title"
                 :holdingTime="searchItem?.holding_time"
